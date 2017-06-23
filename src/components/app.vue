e374bb4bbe9531cb88c4c3059c1bdbaf170feb17
zhangxiang958 <958033967@qq.com>
2017-06-23 16:41:38 +0800

    create repo

---
+++ src/components/app.vue
@@ -0,0 +1,35 @@
+<script>
+    import WaterFall from './waterfall.vue';
+
+    export default {
+        data() {
+            return {
+                items: [1,2,3,4,5]
+            }
+        },
+        components: {
+            WaterFall
+        }
+    }
+</script>
+
+<template>
+    <div class="app">
+        <WaterFall>
+            <div v-for="item in items" class="item" v-if="items">
+                {{ item }}
+            </div>
+        </WaterFall>
+    </div>
+</template>
+
+<style scoped>
+    .item {
+        margin: 5px;
+        width: 200px;
+        height: 300px;
+        line-height: 300px;
+        background: #ccc;
+        text-align: center;
+    }
+</style>
---
+++ src/components/waterfall.vue
@@ -0,0 +1,36 @@
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        created() {
+            console.log('water created');         
+        },
+        mounted() {
+            console.log('water mounted');
+        }
+
+    }
+</script>
+
+<template>
+    <div id="waterfall">
+        <slot>
+            <div class="loading">
+                loading...
+            </div>
+        </slot>
+    </div>
+</template>
+
+<style scoped>
+    #waterfall {
+
+    }
+
+    #waterfall .loading {
+        
+    }
+</style>
